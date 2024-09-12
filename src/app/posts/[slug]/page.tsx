fd4be83b1da3467ffb704dc92d26adeabc1fe1d0
Jihyeon <annawlgus1004@gmail.com>
2024-09-12 10:19:59 +0900

    Prev&Next 포스트 카드

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -2,6 +2,8 @@ import {getPostData} from "@/service/posts";
 import MarkdownViewer from "@/components/MarkdownViewer";
 import Image from "next/image";
 import {AiTwotoneCalendar} from 'react-icons/ai';
+import PostContent from '@/components/PostContent';
+import AdjacentPostCard from "@/components/AdjacentPostCard";
 
 type Props = {
     params: {
@@ -12,7 +14,8 @@ export default async function PostPage({params: {slug}}: Props) {
     // 1. 전달된 slug에 해당하는 포스트 데이터를 읽어와서
     // 2. 데이터를 마크다운뷰어에 표기하면 됨
 
-    const {title, description, date, path, content} = await getPostData(slug);
+    const post = await getPostData(slug);
+    const {title, path, next, prev} = post;
 
     return(
         <article className='rounded-2xl overflow-hidden bg-gray-100 shadow-lg m-4'>
@@ -22,15 +25,10 @@ export default async function PostPage({params: {slug}}: Props) {
                 alt={title} width={760} height={420}
                 unoptimized
             />
-            <section className='flex flex-col p-4'>
-                <div className='flex flex-col p-4 self-end text-sky-600'>
-                    <AiTwotoneCalendar/>
-                    <p className='font-semibold ml-2'>{date.toString()}</p>
-                </div>
-                <h1 className='text-4xl font-bold'>{title}</h1>
-                <p className='text-xl font-bold'>{description}</p>
-                <div className='w-44 border-2 border-sky-600 mt-4 mb-8' />
-                <MarkdownViewer content={content}/>
+            <PostContent post={post}/>
+            <section className='flex shadow-md'>
+                {prev && <AdjacentPostCard post={prev} type='prev'/>}
+                {next && <AdjacentPostCard post={next} type='next'/>}
             </section>
         </article>
     )
